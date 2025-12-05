5cc2fa00fe1ec22e102c5cdc85059c4cb4d501a5
ZojilamIT <manhmomatic@gmail.com>
2025-12-05 12:10:28 +0700

    fix for db

--- aura-backend/main.py
+++ aura-backend/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 origins = ["http://localhost:5173"]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
